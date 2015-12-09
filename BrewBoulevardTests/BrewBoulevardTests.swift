3f5bf08753dfd9abe774e6146a2f49d9dd0cfae5
Laura Lodewyk <llodewyk@andrew.cmu.edu>
2015-12-09 16:53:03 -0500

    committing to git

---
+++ BrewBoulevardTests/BrewBoulevardTests.swift
@@ -0,0 +1,36 @@
+//
+// BrewBoulevardTests.swift
+//  BrewBoulevardTests
+//
+//  Created by 2015 Laura Lodewyk on 12/01/15.
+//  Copyright (c) 2015 Laura Lodewyk. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class BrewBoulevardTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
